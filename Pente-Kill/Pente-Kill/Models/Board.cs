45401fcbe42b3c055e99fe858dc975036dfdb206
Bradly Hedge <BHollandHedge@student.neumont.edu>
2017-11-20 16:55:53 -0700

    Added Board Object CLass
    read title

---
+++ Pente-Kill/Pente-Kill/Models/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente_Kill.Models
+{
+    class Board
+    {
+        private int size;
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public int[,] MakeBoard(int size)
+        {
+            this.Width = size;
+            this.Height = size;
+            int[,] board = new int[Width, Height];
+            return board;
+        }
+
+
+    }
+}

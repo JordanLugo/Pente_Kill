0a51876fe6ae040c9134991b5ef0094fe9ec9468
Unknown <isaiahcjc5@gmail.com>
2017-11-27 22:10:09 -0700

    Dubed some of the tests

--- Pente-Kill/PenteKillTests/UnitTesting.cs
+++ Pente-Kill/PenteKillTests/UnitTesting.cs
@@ -12,22 +12,23 @@ namespace PenteKillTests
 
         [TestMethod]
         public void TestBlackTilePlacement(){
+            //will test if the black tile can be placed in the middle of the board in 4 spots
         }
-        public void TestWhiteTilePlacement()
-        {
+        public void TestWhiteTilePlacement(){
+            //will test if the white tile can be placed on any part of the board 
         }
-        public void TestSave()
-        {
+        public void TestSave(){
+            //will test if the saved game will load in the state it was saved
         }
-        public void TestLoad()
-        {
+        public void TestLoad(){
+            //will test if game will load
         }
-        public void TestBoardSize8()
-        {
+        public void TestBoardSize8(){
+            //sets the board size to 8
         }
 
-        public void TestBoardSize40()
-        {
+        public void TestBoardSize40(){
+            //sets the board size to 40
         }
     }
 }
